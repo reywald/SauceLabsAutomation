a3c2e8d15b5530391f29ea4e021c12314fb353d6
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-07-05 12:12:48 +0100

    Refactor Code: Order Summary Page

--- CommodityPurchaseTest.cs
+++ CommodityPurchaseTest.cs
@@ -69,32 +69,12 @@ public class TShirtPurchaseTest
         checkoutPage.fillForm("Ikechukwu", "Agbarakwe", "112102");
         checkoutPage.Continue();
 
-        // Verify Order Summary page
-        // Assert.IsTrue(driver.Url.Contains("checkout-step-two.html"));
-        // Assert.IsTrue(driver.FindElement(By.ClassName("title")).Text.Contains("Checkout: Overview"));
+        // Verify Order Summary page and review checkout details
+        OrderSummaryPage orderSummaryPage = new(driver, product);
+        orderSummaryPage.VerifyPage();
 
-        // string checkout_qty = driver.FindElement(By.ClassName("cart_quantity")).Text;
-        // string checkout_name = driver.FindElement(By.ClassName("inventory_item_name")).Text;
-        // string checkout_description = driver.FindElement(By.ClassName("inventory_item_desc")).Text;
-        // string checkout_price = driver.FindElement(By.ClassName("inventory_item_price")).Text;
-        // string checkout_price_total = driver.FindElement(By.ClassName("summary_subtotal_label")).Text;
-        // string checkout_tax = driver.FindElement(By.ClassName("summary_tax_label")).Text;
-        // string checkout_total = driver.FindElement(By.ClassName("summary_total_label")).Text;
-
-        // // Review checkout details
-        // Assert.AreEqual(checkout_qty, cart_qty);
-        // Assert.AreEqual(checkout_name, item_name);
-        // Assert.AreEqual(checkout_description, item_description);
-        // Assert.AreEqual(checkout_price, item_price);
-        // Assert.IsTrue(checkout_price_total.Contains(checkout_price));
-
-        // double itemPrice = Convert.ToDouble(checkout_price.Substring(1));
-        // double tax = Convert.ToDouble(checkout_tax.Substring(6));
-        // double total = Convert.ToDouble(checkout_total.Substring(8));
-        // Assert.IsTrue((itemPrice + tax) == total);
-
-        // // Finalize Purchase
-        // driver.FindElement(By.Id("finish")).Click();
+        // Finalize Purchase
+        orderSummaryPage.Finish();
 
         // // Verify Order Summary page
         // Assert.IsTrue(driver.Url.Contains("checkout-complete.html"));
---
+++ pages/OrderSummaryPage.cs
@@ -0,0 +1,70 @@
+using OpenQA.Selenium;
+
+namespace SauceLabsAutomation
+{
+    internal class OrderSummaryPage : BasePage
+    {
+
+        public OrderSummaryPage(IWebDriver driver, Product product) : base(driver)
+        {
+            this.product = product;
+            this.SITE_URL += "checkout-step-two.html";
+        }
+
+        Product product;
+
+        readonly By title = By.ClassName("title");
+        readonly By checkoutQty = By.ClassName("cart_quantity");
+        readonly By checkoutName = By.ClassName("inventory_item_name");
+        readonly By checkoutDescription = By.ClassName("inventory_item_desc");
+        readonly By checkoutPrice = By.ClassName("inventory_item_price");
+        readonly By checkoutPriceTotal = By.ClassName("summary_subtotal_label");
+        readonly By checkoutTax = By.ClassName("summary_tax_label");
+        readonly By checkoutTotal = By.ClassName("summary_total_label");
+        readonly By finishButton = By.Id("finish");
+
+        public void Finish()
+        {
+            GetElement(finishButton).Click();
+        }
+
+        public override void VerifyPage()
+        {
+            Assert.AreEqual(SITE_URL, webDriver.Url);
+            Assert.AreEqual("Swag Labs", webDriver.Title);
+            Assert.IsTrue(GetElement(title).Displayed);
+            Assert.AreEqual(GetElement(title).Text, "Checkout: Overview");
+
+            Assert.IsTrue(GetElement(checkoutQty).Displayed);
+            Assert.AreEqual(GetElement(checkoutQty).Text, product.quantity);
+
+            Assert.IsTrue(GetElement(checkoutName).Displayed);
+            Assert.AreEqual(GetElement(checkoutName).Text, product.name);
+
+            Assert.IsTrue(GetElement(checkoutDescription).Displayed);
+            Assert.AreEqual(GetElement(checkoutDescription).Text, product.description);
+
+            Assert.IsTrue(GetElement(checkoutPrice).Displayed);
+            Assert.AreEqual(GetElement(checkoutPrice).Text, product.price);
+
+            Assert.IsTrue(GetElement(checkoutPriceTotal).Displayed);
+            Assert.IsTrue(GetElement(checkoutPriceTotal).Text.Contains(product.price));
+
+            Assert.IsTrue(GetElement(checkoutTax).Displayed);
+
+            Assert.IsTrue(GetElement(checkoutTotal).Displayed);
+            VerifyCost();
+
+            Assert.IsTrue(GetElement(finishButton).Displayed);
+            // Assert.IsTrue(GetElement(cart).Text.Length == 0);
+        }
+
+        private void VerifyCost()
+        {
+            double itemPrice = Convert.ToDouble(GetElement(checkoutPrice).Text.Substring(1));
+            double tax = Convert.ToDouble(GetElement(checkoutTax).Text.Substring(6));
+            double total = Convert.ToDouble(GetElement(checkoutTotal).Text.Substring(8));
+            Assert.IsTrue((itemPrice + tax) == total);
+        }
+    }
+}
